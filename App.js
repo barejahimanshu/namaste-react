90f8ddeac31e8ceba1a2783e37f73223c4610945
Himanshu Bareja <himanshu.bareja465@gmail.com>
2026-06-08 16:43:19 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,42 @@
+// Structure I need to create
+
+{
+  /* <div id="parent">
+    <div id="child1">
+        <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
+    </div>
+<div id="child2">
+        <h1>I am h1 tag of child2</h1>
+        <h2>I am h2 tag of child2</h2>
+    </div>
+</div> */
+}
+const el = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {}, "I am h1 tag"),
+      React.createElement("h2", {}, "I am h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am h1 tag of child2"),
+      React.createElement("h2", {}, "I am h2 tag of child2"),
+    ]),
+  ],
+);
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "Hello World from React",
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(el);
